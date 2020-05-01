b50c15e137d3e4cb4d223dd680b2e1b5b48d4db1
Astroglia <42473606+Astroglia@users.noreply.github.com>
2020-05-01 12:03:06 -0500

    diagnosis changes

--- Visualization/singlePlot.py
+++ Visualization/singlePlot.py
@@ -2,35 +2,33 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 import numpy as np 
 
+import matplotlib
+print(matplotlib.get_backend())
 class singlePlot:
-
-    def __init__(self, features, motion_data, feature_count):
-#         if len(features) != len(motion_data):
-#             raise ValueError('labels must be the same size as features')
+    def __init__(self, features, motion_data, feature_count, data_labels=None, motion_labels=None):
         self.features = features # list of [ 10, 224 ] matrices
         self.feature_count = feature_count
+        self.default_channel_plot() #automatically plot all channels
+        self.motion_data = motion_data # list of [ 4, 5 ] matrices.
         self.data_to_plot = None
         self.motion_data_to_plot = None
-        self.default_channel_plot()
-        self.motion_data = motion_data # list of [ 4, 5 ] matrices.
+        self.x_axis = None
+        if motion_labels is None:   self.motion_labels = ['thumb', 'index','middle','ring','pinky']
+        else:                       self.motion_labels = motion_labels
+        if data_labels is None:     self.data_labels = ['ZC', 'SSC', 'WL', 'WAMP', 'MAB', 'MSQ', 'RMS', 'V3', 'LGDEC', 'DABS','MFL','MPR','MAVS','WMAB']
+        else:                       self.data_labels = data_labels
 
-    def plot_data(self):
-        self.data_to_plot = np.moveaxis(self.get_data_to_plot()[0,:,:], 0, 1 )
+    def plot_motion_and_features(self):
+        self.data_to_plot = np.moveaxis(self.get_data_to_plot(), 0, 1 )
         self.motion_data_to_plot = np.moveaxis(np.dstack(self.motion_data)[0,:,:], 0 , 1 )
-        self.x_axis_time = range(0, len(self.data_to_plot))
-        print(self.data_to_plot.shape)
-        print(self.motion_data_to_plot.shape)
-       # plt.style.use('dark_background') #pretty
-        figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-        #plt.plot(self.data_to_plot)
-        self.motion_data_to_plot = self.motion_data_to_plot*self.data_to_plot.max() #scale to max
-        plt.fill_between(self.x_axis_time, self.motion_data_to_plot[:,0], 0, alpha=0.5, label='thumb')
-        plt.fill_between(self.x_axis_time, self.motion_data_to_plot[:,1], 0, alpha=0.5, label='index')
-        plt.fill_between(self.x_axis_time, self.motion_data_to_plot[:,2], 0, alpha=0.5, label='middle')
-        plt.fill_between(self.x_axis_time, self.motion_data_to_plot[:,3], 0, alpha=0.5, label='ring')
-        plt.fill_between(self.x_axis_time, self.motion_data_to_plot[:,4], 0, alpha=0.5, label='pinky')
-        plt.legend(loc="upper left")
-        plt.plot(self.data_to_plot)
+        self.x_axis = range(0, len(self.data_to_plot))
+        self.motion_data_to_plot = self.motion_data_to_plot*self.data_to_plot.max() # set feature data and motion data on the same scales.
+
+        for i, label in enumerate(self.motion_labels):
+            plt.fill_between(self.x_axis, self.motion_data_to_plot[:, i], alpha=0.5, label=label)
+        for i, label in enumerate(self.data_labels):
+            plt.plot(self.data_to_plot[:,i], alpha=0.8, label=label)
+        plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
         plt.show()
         
     def get_data_to_plot(self):
@@ -43,7 +41,7 @@ class singlePlot:
                     channel = j
                     temp.append( most_recent[ int(channel*self.feature_count):(int(channel*self.feature_count + self.feature_count))])
             data_to_plot.append( np.dstack(temp) )
-        return np.dstack(data_to_plot)
+        return np.dstack(data_to_plot)[0,:,:]
         
     def default_channel_plot(self):
         total_channels = int(self.features[0].shape[1] / self.feature_count)
--- main.py
+++ main.py
@@ -6,17 +6,19 @@ from LeapMotionConfig import subSampling, modifications
 
 # loads all pickled data from data folder. return list of data
 unconfigured_data = dataLoad.load_directory_data('./data_folder/current')
-for key, value in unconfigured_data[0].items():
-    print (key)
 
+print("KEYS IN DATA: ")
+for key, value in unconfigured_data[0].items(): print (key)
+
+#unwrap data (database storage requires certain formats for data, so we need to do this)
 feature_matrices = dataLoad.batch_unwrap_feamats( unconfigured_data )
-motion_data = dataLoad.batch_unwrap_motion_data( unconfigured_data )
-time_deltas = dataLoad.batch_unwrap_time_deltas( unconfigured_data )
+motion_data      = dataLoad.batch_unwrap_motion_data( unconfigured_data )
+time_deltas      = dataLoad.batch_unwrap_time_deltas( unconfigured_data )
 
 #just use data from a single file for example
 feature_matrices = feature_matrices[0]
-motion_data = motion_data[0] 
-time_deltas = time_deltas[0]
+motion_data      = motion_data[0] 
+time_deltas      = time_deltas[0]
 
 #convert all motion data list of lists to a single list. Downsample each list to nearest 5 degrees.
 motion_data_processed = []
@@ -35,22 +37,14 @@ for i, matrix_list in enumerate(motion_data):
         motion_data_processed.append(matrix)
 
     else: #motion data was empty (bug in data collection). Zero-out associated feature matrix, create dummy motion data.
-        
         motion_data_processed.append( subSampling.get_LeapMotion_zero_matrix() )
         feature_matrices[i] = 0*feature_matrices[i]
 
-# print one of each index
-print( motion_data_processed[1160] )
-print( feature_matrices[1160] )
-
-# for i in range(500, 2500):
-#      print(i, " " , motion_data_processed[i])
-
-#print length for verification
-print(len(motion_data_processed))
+print("list lengths -- these should be the same :")
 print(len(feature_matrices))
-from Visualization import singlePlot
+print(len(motion_data_processed))
 
+from Visualization import singlePlot
 plot_object = singlePlot.singlePlot(features=feature_matrices, motion_data=motion_data_processed, feature_count=14)
-plot_object.select_channels([ 10 ]) #plot channels from first channel.
-plot_object.plot_data()
+plot_object.select_channels([ 10 ]) #plot features calculated from the 10th channel in recording.
+plot_object.plot_motion_and_features()
